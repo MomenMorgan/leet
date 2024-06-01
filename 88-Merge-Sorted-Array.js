e343f0aa3fac43395d6210c7c3280dd38ef6bea2
Momen Morgan <117030094+MomenMorgan@users.noreply.github.com>
2024-06-01 11:43:43 +0300

    [88] [Time Beats: 91.96%] [Memory Beats: 29.39%] - LeetPush

--- 88-Merge-Sorted-Array.js
+++ 88-Merge-Sorted-Array.js
@@ -6,31 +6,20 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
-    let len = m + n
-    let swapped;
-    let j = 0;
-    nums1.length = nums1.length - n 
-    do {
-        
-        if (j < n) {
-            nums1.push(nums2[j])
-            j++;
+    let i = m - 1
+    let j = n - 1
+    let k = m + n - 1
+
+    while (j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i]
+            i--;
+        } else  {
+            nums1[k] = nums2[j]
+            j--;
         }
-        swapped = false;
-        for (let i = 0; i < len ; i++) {
+        k--;
+    }
 
-            if (nums1[i] > nums1[i + 1]) {
-                let temp = nums1[i]
-                nums1[i] = nums1[i + 1]
-                nums1[i + 1] = temp
-                swapped = true
-            }
-
-        }
-
-    } while (swapped || j < n)
-
-    
-
-
+  
 };
