cdae10600f6cff6c96beb23dee9c151fbbabfc47
Momen Morgan <117030094+MomenMorgan@users.noreply.github.com>
2024-10-30 17:32:27 +0300

    [26] [Time Beats: 100.00%] [Memory Beats: 13.45%] - LeetPush

---
+++ 26-Remove-Duplicates-from-Sorted-Array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0;
+
+let uniqueIndex = 0;
+    for(let i = 1; i < nums.length; i++){
+        if(nums[uniqueIndex] !== nums[i]){
+        uniqueIndex++
+            nums[uniqueIndex] = nums[i]
+        }
+    }
+    return uniqueIndex + 1
+   
+};
